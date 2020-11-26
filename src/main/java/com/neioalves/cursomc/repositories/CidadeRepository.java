596fbf127690e82ddcdd5cdf24c515ff8d3cc3ae
thiago <thiagors20@hotmail.com>
2020-11-26 14:30:40 -0300

    Estado e Cidade

---
+++ src/main/java/com/neioalves/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.neioalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.neioalves.cursomc.domain.Cidade;
+
+@Repository 
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
