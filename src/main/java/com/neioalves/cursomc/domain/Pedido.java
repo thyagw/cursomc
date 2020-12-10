9e40d504b82c071a08bf63e3c0cf30e07826b0ab
thiago <thiagors20@hotmail.com>
2020-12-10 13:36:24 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/neioalves/cursomc/domain/Pedido.java
+++ src/main/java/com/neioalves/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.neioalves.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -31,7 +34,10 @@ public class Pedido implements Serializable{
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
-
+	
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Pedido() {}
 
 	public Pedido(Integer id, Date instate, Cliente cliente, Endereco enderecoDeEntrega) {
@@ -106,6 +112,14 @@ public class Pedido implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 	
 	
---
+++ src/main/java/com/neioalves/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.neioalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.neioalves.cursomc.domain.ItemPedido;
+
+@Repository 
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+
+}
